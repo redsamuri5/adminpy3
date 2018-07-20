723348b61a229f35484876287f46d7a5df87c308
Redsamuri <34795680+Redsamuri@users.noreply.github.com>
2018-07-20 11:22:07 +0700

    Update config.py

--- LINEPY/config.py
+++ LINEPY/config.py
@@ -27,14 +27,14 @@ class Config(object):
         'LINE_SERVICES': '1459630796'
     }
 
-    APP_TYPE    = ApplicationType._VALUES_TO_NAMES[368]
-    APP_VER     = '8.4.2'
+    APP_TYPE    = "CHROMEOS\t2.1.0\tCHROMEOS\t10.0.0"
+    APP_VER     = '8.9.1'
     CARRIER     = '51089, 1-0'
-    SYSTEM_NAME = 'CLOVAFRIENDS_RC'
+    SYSTEM_NAME = 'CHROMEOS'
     SYSTEM_VER  = '10.12.0'
     IP_ADDR     = '8.8.8.8'
     EMAIL_REGEX = re.compile(r"[^@]+@[^@]+\.[^@]+")
 
     def __init__(self):
-        self.APP_NAME = '%s\t%s\t%s\t%s' % (self.APP_TYPE, self.APP_VER, self.SYSTEM_NAME, self.SYSTEM_VER)
+        self.APP_NAME = 'CHROMEOS\t2.1.0\tCHROMEOS\t10.0.0'
         self.USER_AGENT = 'Line/%s' % self.APP_VER
